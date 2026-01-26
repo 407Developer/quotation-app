4f0f76dcd5cf4c47ee4f5249e823ea2ec1f115a7
Victor Ajibade <ajibadevic@gmail.com>
2026-01-26 15:24:41 +0100

    fix: update sw cache

--- sw.js
+++ sw.js
@@ -1,9 +1,15 @@
-const CACHE_NAME = "quotation-app-v1";
+const CACHE_NAME = "quotation-app-v2";
 const ASSETS = [
   "/",
   "/index.html",
   "/style.css",
   "/script.js",
+  "/js/calc.js",
+  "/js/model.js",
+  "/js/rules.js",
+  "/js/state.js",
+  "/js/storage.js",
+  "/js/ui.js",
   "/manifest.json"
 ];
 
@@ -11,6 +17,16 @@ self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
